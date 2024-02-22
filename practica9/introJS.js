847afbd411f7fa17736b712ee65949df28fd4ccd
dhema22 <93835625+dhema22@users.noreply.github.com>
2024-02-22 15:18:56 +0100

    Add files via upload

---
+++ practica9/introJS.js
@@ -0,0 +1,67 @@
+//salidas consola
+console.log("hola mundo desde js");
+console.warn("advertencia en consola");
+console.error("esto es una prueba de error");
+
+agenda=[];
+agenda[0]="adhemar";
+agenda[1]="quinteiro";
+console.table(agenda);
+
+//operadores aritmeticos
+console.log(7+3);
+console.log(7-3);
+console.log(7*3);
+console.log(7/3);
+console.log(7%3);
+
+//operadores comparación
+console.log(7<3);
+console.log(7<=3);
+console.log(7>3);
+console.log(7>=3);
+console.log(7==3);
+console.log(7!=3);
+console.log("3"===3);
+
+//booleans
+console.log(2==2 && 3==3);
+console.log(2==1 || 3==3);
+
+//variables 
+var edad2=45;
+console.log("tienes: ");
+console.log(edad);
+
+//encadenar 
+console.log("tienes "+edad+" años");
+
+//incremento y decremento
+edad++;
+console.log(edad);
+edad--;
+console.log(edad);
+
+//operadores abreviados 
+var edad=20;
+edad +=2; 
+console.log(edad);
+edad -=5;
+console.log(edad);
+edad *=2;
+console.log(edad);
+edad /=2;
+console.log(edad);
+
+ /*Esto es un comentario
+  multilinea
+  Esto es otra linea diferente 
+  */
+
+//tipos de variables
+var nombre="adhemar"
+nombre="adhemar"
+//la diferencia entre var y let es que let es una variable local 
+let segundo="jeronimo"
+const diasSemana=7;
+diasSemana++;
